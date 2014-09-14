ac112d745bd9d922ab16488249540358d7684eb3
Alejandro Bustamante <a.bustamante@erunner.eu>
2014-09-14 13:57:19 +0200

    started addition of RSVP to public side of app

--- src/Party/Bundle/PublicBundle/Controller/HomeController.php
+++ src/Party/Bundle/PublicBundle/Controller/HomeController.php
@@ -2,12 +2,98 @@
 
 namespace Party\Bundle\PublicBundle\Controller;
 
+use Party\Bundle\PublicBundle\Form\AttendeeType;
+use Party\Bundle\PrivateBundle\Entity\Attendee;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('PartyPublicBundle:Default:index.html.twig', array('name' => 'temp'));
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('PartyPrivateBundle:Party')->findAll();
+
+        return $this->render('PartyPublicBundle:Home:index.html.twig', array(
+                'entities' => $entities,
+            ));
     }
+
+    /**
+     * Creates a new Attendee entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Attendee();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            /*
+                        $party = $em->getRepository('PartyPrivateBundle:Party')->find($request->query->get('id'));
+
+                        $enty->addParty($party);
+
+                        /*$previousAttendee = $em->getRepository('PartyPrivateBundle:Attendee')->findOneBy(array('email' => $form->getData()['email']));
+
+                        if ($previousAttendee) {
+            
+                        }*/
+
+            $em->persist($entity);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add(
+                'flashMessage',
+                array('state' => 'success','message' => 'RSVP Confirmed!')
+            );
+
+            return $this->redirect($this->generateUrl('public_index'));
+        }
+
+        return $this->render('PartyPublicBundle:Attendee:new.html.twig', array(
+                'entity' => $entity,
+                'form'   => $form->createView(),
+            ));
+    }
+
+    /**
+     * Creates a form to create a Attendee entity.
+     *
+     * @param Attendee $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Attendee $entity)
+    {
+        $form = $this->createForm(new AttendeeType(), $entity, array(
+                'action' => $this->generateUrl('attendee_create'),
+                'method' => 'POST',
+            ));
+
+        $form->add('submit', 'submit', array('label' => 'Attend party'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Attendee entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $entity = new Attendee();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('PartyPublicBundle:Attendee:new.html.twig', array(
+                'entity' => $entity,
+                'form'   => $form->createView(),
+            ));
+    }
+
+
 }
---
+++ src/Party/Bundle/PublicBundle/Form/AttendeeType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Party\Bundle\PublicBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AttendeeType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Party\Bundle\PrivateBundle\Entity\Attendee'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'party_bundle_privatebundle_attendee';
+    }
+}
