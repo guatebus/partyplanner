cf9aae81f0c94df43c69c1adcdb4441869f4ee08
Alejandro Bustamante <a.bustamante@erunner.eu>
2014-09-14 09:28:58 +0200

    added private and public bundles

---
+++ src/Party/Bundle/PrivateBundle/PartyPrivateBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Party\Bundle\PrivateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PartyPrivateBundle extends Bundle
+{
+}
---
+++ src/Party/Bundle/PublicBundle/PartyPublicBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Party\Bundle\PublicBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PartyPublicBundle extends Bundle
+{
+}
